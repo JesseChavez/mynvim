f99969a937a7faab9cab3dee11d2167037b6856f
Jesse Chavez <jesse.chavez.r@gmail.com>
2026-02-07 08:31:13 +0900

    view linting message in floating panel with space-e

--- init.lua
+++ init.lua
@@ -196,6 +196,7 @@ vim.diagnostic.config {
 }
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
